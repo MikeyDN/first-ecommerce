e15769026ac035228a0602074ac60dffb1337abc
MikeyDN <Mikeydn24@gmail.com>
2023-01-07 15:05:44 +0200

    fix useless import

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import Footer from './Footer'
-import Header from './Header'
 import TopNavbar from './nav/TopNavbar'
 import BotNavbar from './nav/BotNavbar'
 import Cart from './Cart'
