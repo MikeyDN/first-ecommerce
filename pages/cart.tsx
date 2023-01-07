a8a6d51bd5fadad5368e570effe43ac7e879164f
MikeyDN <Mikeydn24@gmail.com>
2023-01-07 16:53:13 +0200

    modified:   pages/cart.tsx 	modified:   styles/global.css

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -2,7 +2,7 @@ import { useCart } from 'react-use-cart'
 import { useEffect, useRef } from 'react'
 import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { faTrash, faAdd, faMinus } from '@fortawesome/free-solid-svg-icons'
 import Image from 'next/image'
 import { motion } from 'framer-motion'
 import { urlFor } from '../lib/client'
@@ -17,6 +17,7 @@ function cartPageComponent() {
     updateItemQuantity,
     removeItem,
     cartTotal,
+    emptyCart,
   } = useCart()
 
   return (
@@ -27,40 +28,44 @@ function cartPageComponent() {
           <ul>
             {items.map((item, index) => (
               <li key={index}>
-                <div className="cart-item-image">
-                  <Image
-                    src={urlFor(item.image[0]).width(150).height(150).url()}
-                    alt={item.id}
-                    fill
-                    sizes="(min-width: 1080px) 300px,
+                <div className="cart-item-container">
+                  <div className="cart-item-image">
+                    <Image
+                      src={urlFor(item.image[0]).width(300).height(300).url()}
+                      alt={item.id}
+                      fill
+                      sizes="(min-width: 1080px) 300px,
                             1080px"
-                  />
-                </div>
-
-                <div className="cart-item-details">
-                  <div className="cart-item-name">
-                    <h4>{item.name}</h4>
+                    />
                   </div>
-                  <div className="cart-item-price">Price: {item.price}$</div>
-                  <div className="cart-item-quantity">Qty: {item.quantity}</div>
+                  <div className="cart-item-details">
+                    <div className="cart-item-name">
+                      <h4>{item.name}</h4>
+                    </div>
+                    <div className="cart-item-price">Price: {item.price}$</div>
+                    <div className="cart-item-quantity">
+                      Qty: {item.quantity}
+                    </div>
+                  </div>
                 </div>
-
                 <div className="cart-item-control">
                   <button
                     onClick={() =>
                       updateItemQuantity(item.id, item.quantity! - 1)
                     }
                   >
-                    -
+                    <FontAwesomeIcon icon={faMinus} />
                   </button>
                   <button
                     onClick={() =>
                       updateItemQuantity(item.id, item.quantity! + 1)
                     }
                   >
-                    +
+                    <FontAwesomeIcon icon={faAdd} />
                   </button>
-                  <button onClick={() => removeItem(item.id)}>&times;</button>
+                  <button onClick={() => removeItem(item.id)}>
+                    <FontAwesomeIcon icon={faTrash} />
+                  </button>
                 </div>
               </li>
             ))}
